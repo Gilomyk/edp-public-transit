14490d32e8b943b12d6a3d6ad2f8ea2478f741aa
Gilomyk <gilomyk@gmail.com>
2025-06-05 21:03:57 +0200

    Added local SQLite storage for likes and search history

--- src/main/java/com/example/projectedp/MainApp.java
+++ src/main/java/com/example/projectedp/MainApp.java
@@ -3,35 +3,46 @@ package com.example.projectedp;
 import com.example.projectedp.controller.MainController;
 import com.example.projectedp.event.*;
 import com.example.projectedp.event.handler.*;
+import com.example.projectedp.service.*;
+import com.example.projectedp.model.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.*;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainApp extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/main.fxml"));
         Parent root = loader.load();
+
+        DatabaseService db = new DatabaseService();
+        db.init();
 
         // Przekazanie EventBus do kontrolera
         MainController controller = loader.getController();
         EventBus eventBus = new EventBus();
         controller.setEventBus(eventBus);
 
+        List<Stop> favs = db.getAllFavorites();
+        controller.updateFavoritesList(favs);
+
         // Rejestracja handlerów
         eventBus.register(DeparturesLoadedEvent.class, new DeparturesLoadedHandler());
         eventBus.register(NotificationRequestedEvent.class, new NotificationRequestedHandler());
         eventBus.register(StopSelectedEvent.class, new StopSelectedHandler());
         eventBus.register(ApiErrorEvent.class, new ApiErrorHandler());
-        eventBus.register(StopSearchRequestedEvent.class, new StopSearchRequestedHandler(controller));
-        eventBus.register(StopAddedToFavoritesEvent.class, new StopAddedToFavoritesHandler());
+        eventBus.register(StopSearchRequestedEvent.class, new StopSearchRequestedHandler(db, controller));
+        eventBus.register(StopAddedToFavoritesEvent.class, new StopAddedToFavoritesHandler(db, controller));
+        eventBus.register(StopRemovedFromFavoritesEvent.class, new StopRemovedFromFavoritesHandler(db, controller));
 
         stage.setTitle("Rozkład jazdy 3000");
-        stage.setScene(new Scene(root, 800, 600));
+        stage.setScene(new Scene(root, 1000, 600));
         stage.show();
     }
 
